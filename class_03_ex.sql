975bc469a9af4aa3b08238b420cabfe0586e1bb1
DESKTOP-D <leeedanny1@gmail.com>
2022-05-02 17:55:54 +0900

    database 기초

--- class_03_ex.sql
+++ class_03_ex.sql
@@ -2,10 +2,51 @@
 
 use shopdb_1;
 
-create database shopdb_1;
+create database shopdb_2;
 
 -- 테이블 생성
+create table userTBL(
+	userName char(3) not null,
+    birthYear int not null,
+    addr char(2) not null,
+    mobile char(12),
+    primary key(userName)
+);
 
+-- 필드 수정, 컬럼 수정
+alter table userTBL modify mobile char(13);
+desc userTBL;
+
+-- 구매 테이블 생성 (fk)
+create table buyTBL(
+	userName char(3) not null,
+    prodName char(3) not null,
+    price int not null,
+    amount int not null,
+    foreign key(userName) references userTBL(userName)
+);
+
+desc buyTBL;
 -- 연관 관계 생성
+-- userTBL 데이터 입력하기
+insert into userTBL values('이승기', 1987, '서울', '010-1234-1234');
+insert into userTBL values('홍길동', 1911, '부산', '010-2222-2222');
+insert into userTBL values('이순신', 1999, '대구', '010-3333-3333');
 
--- C R U D 연습 
+select * from userTBL;
+select * from buyTBL;
+
+-- 구매 테이블 데이터 입력
+-- insert into buytbl values('이승기2', '운동화', 50, 1);
+
+insert into buyTBL values('이승기', '운동화', 50, 1);
+insert into buyTBL values('이승기', '노트북', 150, 1);
+insert into buyTBL values('홍길동', '책', 10, 5);
+insert into buyTBL values('홍길동', '모니터', 200, 2);
+insert into buyTBL values('이순신', '청바지', 40, 1);
+insert into buyTBL values('이순신', '책', 10, 3);
+
+-- 
+select * from userTBL;
+select * from buyTBL;
+-- C R U D 연습
